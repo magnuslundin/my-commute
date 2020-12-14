dce763102564bf889cca228acc91c5b593f3c3fa
Magnus Lundin <magnus.lundin@gmail.com>
2020-12-14 20:25:14 +0100

    Disable global loading of icons

--- mc-frontend/src/main.js
+++ mc-frontend/src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import store from './store/store'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
